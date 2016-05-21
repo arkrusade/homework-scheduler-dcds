f69269c84a0e3d27d4c7fb862ca815a0ef7ccd6c
arenbecl <carenberg@dcds.edu>
2016-05-21 17:18:13 -0400

    Update date.java

--- src/homework_scheduler/date.java
+++ src/homework_scheduler/date.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package homework_scheduler;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 /**
  *
  * @author Justin Lee
  */
 public class date {
+    private int day;
+    private int month;
+    private int year;
     
+    public date(int d, String m, String y){
+        day = d;
+        if (m == "January"){
+            month = 1;
+        }
+         if (m == "January"){
+            month = 1;
+        }
+    }
 }
