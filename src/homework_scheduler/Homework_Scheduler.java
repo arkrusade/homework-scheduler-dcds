b35e162caf2c1364fd38926481cdc9d06f94ab1a
Justin Lee <justinjlee99@gmail.com>
2016-06-16 08:46:09 -0400

    added selenium

--- src/homework_scheduler/Homework_Scheduler.java
+++ src/homework_scheduler/Homework_Scheduler.java
@@ -10,6 +10,8 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,7 +25,6 @@ public class Homework_Scheduler {
      * @param args the command line arguments
      */
     ArrayList<String> classes = new ArrayList(7);
-    static String ending;
     String workingBlockString;
     final static Pattern okBlocks = Pattern.compile("\\((1|2|3|4|5|6|7|8)\\)");
     final static Pattern okPeriods = Pattern.compile("\\(p (1|2|3|4|5|6|7|8)\\)");
@@ -32,36 +33,6 @@ public class Homework_Scheduler {
     public static void main(String[] args) {
         Homework_Scheduler m = new Homework_Scheduler();
 //        m.organize();
-        ending = "\n"
-                + "\n"
-                + "    \n"
-                + "Select Language??\n"
-                + "\n"
-                + "\n"
-                + "DETROIT COUNTRY DAY SCHOOL | CONTACT US\n"
-                + "\n"
-                + "\n"
-                + "Lower School (Grades PK-2)\n"
-                + "3003 West Maple Road\n"
-                + "Bloomfield Hills, MI 48301\n"
-                + "Phone: 248.430.2740 Junior School (Grades 3-5)\n"
-                + "3600 Bradway Boulevard\n"
-                + "Bloomfield Hills, MI 48301\n"
-                + "Phone: 248.430.1074 Middle School (Grades 6-8)\n"
-                + "22400 Hillview Lane\n"
-                + "Beverly Hills, MI 48025\n"
-                + "Phone: 248.430.1677 Upper School (Grades 9-12)\n"
-                + "22305 West 13 Mile Road\n"
-                + "Beverly Hills, MI 48025\n"
-                + "Phone: 248.646.7717\n"
-                + "\n"
-                + "\n"
-                + "Detroit Country Day School is a private, independent, co-educational, non-denominational, preschool through grade 12 college preparatory school focused on a well-rounded liberal arts education. Emphasis on academics, arts, athletics, and character development is prevalent across the curriculum. The school admits students of any race, color, religion, sexual orientation, national and ethnic origin to all the rights, privileges, programs and activities generally accorded or made available to students in the school. \n"
-                + "Privacy Policy\n"
-                + "Full Non-discrimination Statement\n"
-                + "\n"
-                + "powered by finalsite\n"
-                + " Account: Justin Lee  Portal  Groups Dashboard  Bookmarks  Logoff ";
         m.workingBlockString = trim(m.input());
         m.organizeToWorks();
     }
@@ -98,27 +69,31 @@ public class Homework_Scheduler {
         StringBuilder input = new StringBuilder(in);
         input.delete(0, input.indexOf("<<"));
         input.delete(0, "<<  <  ".length());
-        input.delete(input.indexOf(ending), input.length() - 1);
 
         return input.toString();
     }
 
     public String input() {
-        File file = new File("input.txt");
-        StringBuilder input = new StringBuilder();
-        try (Scanner in = new Scanner(file)) {
-            while (in.hasNextLine()) {
-                String i = in.nextLine();
-                input.append(i);
-                input.append("\n");
-            }
-        } catch (FileNotFoundException exception) {
-            System.out.println("File not found");
-        } catch (StringIndexOutOfBoundsException exception) {
-            System.out.println("there was index out of bounds");
+        try {
+            //        File file = new File("input.txt");
+//        StringBuilder input = new StringBuilder();
+//        try (Scanner in = new Scanner(file)) {
+//            while (in.hasNextLine()) {
+//                String i = in.nextLine();
+//                input.append(i);
+//                input.append("\n");
+//            }
+//        } catch (FileNotFoundException exception) {
+//            System.out.println("File not found");
+//        } catch (StringIndexOutOfBoundsException exception) {
+//            System.out.println("there was index out of bounds");
+//        }
+//        
+//        return input.toString();
+            return web.scrape();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Homework_Scheduler.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        return input.toString();
+        return "Failure";
     }
-
 }
--- src/homework_scheduler/web.java
+++ src/homework_scheduler/web.java
@@ -1,18 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package homework_scheduler;
 
-import java.net.*;
-import java.io.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+/**
+ *
+ * @author Guest()
+ */
 public class web {
-    public static void main(String[] args) throws Exception {
 
-        URL oracle = new URL("https://www.dcds.edu/page.cfm?p=8186Z");
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(oracle.openStream()));
+    /**
+     * @param args the command line arguments
+     */
+    public static String scrape() throws InterruptedException {
+        // declaration and instantiation of objects/variables
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Justin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String baseUrl = "http://www.dcds.edu/page.cfm?p=8256&period=week";
+        String expectedTitle = "Detroit Country Day School: Academics Individual Classes Calendar";
+        String actualTitle = "";
+        String inputString, hasLength, classesText;
+        hasLength = "";
+        classesText = "";
+        // launch Chrome and direct it to the Base URL
+        driver.get(baseUrl);
 
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
+        driver.findElement(By.name("username")).sendKeys("lee1801");
+        driver.findElement(By.name("password")).sendKeys("aaruurayhh`");
+        driver.findElement(By.name("submit")).click();
+        driver.get("https://www.dcds.edu/page.cfm?p=8256&start=06/02/2016&period=week");;
+
+        /*
+         * compare the actual title of the page witht the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        // get the actual value of the title
+        try {
+            actualTitle = driver.getTitle();
+//            System.out.println(actualTitle);
+//            if (actualTitle.contentEquals(expectedTitle)) {
+//                System.out.println("Test Passed!");
+//            } else {
+//                System.out.println("Test Failed");
+//            }
+            hasLength = driver.findElement(By.id("calendar_titlebar")).getText();
+//            System.out.println(hasLength);
+
+            classesText = driver.findElement(By.className("listonly_block")).getText();
+//            System.out.println(classesText);
+        } catch (Exception e) {
+            System.err.println("Failed from exception " + e.toString());
+        }
+        Thread.sleep(5000);
+
+        //close Firefox
+        driver.close();
+        return inputString = hasLength + classesText;
+
     }
+
 }
